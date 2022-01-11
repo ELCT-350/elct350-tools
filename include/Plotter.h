d96954d84b35df7a92a496d453da1f0a1f11bce1
josephhood <96262451+josephhood@users.noreply.github.com>
2022-01-10 20:48:43 -0500

    Updated copyright

--- include/Plotter.h
+++ include/Plotter.h
@@ -1,7 +1,7 @@
 /*
 MIT License
 
-Copyright (c) 2017  Joe Hood
+Copyright (c) 2022  Joseph Hood
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
